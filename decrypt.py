a4db1da221e0d835f63e8355f628cb4bb19d2f13
jake.coyne <coyneer080backup@gmail.com>
2024-06-05 17:02:44 -0400

    cookie-editor addition
    Added function to save to json which can be easily copy and pasted into cookie editor

--- decrypt.py
+++ decrypt.py
@@ -34,6 +34,41 @@ def cookies(key, file_location):
 		print(e)
 		sys.exit(1)
 
+def cookies_for_editor(key, file_location):
+    cookies = []
+    if os.path.isfile(file_location) == False:
+        print("Error: File does not exist")
+        sys.exit(1)
+    try:
+        conn = sqlite3.connect(file_location)
+        cursor = conn.cursor()
+        cursor.execute('select host_key, "TRUE", path, "FALSE", expires_utc, name, encrypted_value from cookies')
+        values = cursor.fetchall()
+        for host_key, _, path, _, expires_utc, name, encrypted_value in values:
+            decrypted_value = decrypt_data(encrypted_value, key)
+            expiration_date = (datetime(1601, 1, 1) + timedelta(microseconds=expires_utc)).timestamp()
+            cookie = {
+                "domain": host_key,
+                "expirationDate": expiration_date,
+                "hostOnly": not host_key.startswith('.'),
+                "httpOnly": False,
+                "name": name,
+                "path": path,
+                "sameSite": None,
+                "secure": False,
+                "session": False,
+                "storeId": None,
+                "value": decrypted_value
+            }
+            cookies.append(cookie)
+        with open('cookies_for_cookie_editor.json', 'w') as f:
+            json.dump(cookies, f, indent=4)
+        print("Cookies saved to cookies_for_cookie_editor.json")
+    except sqlite3.Error as e:
+        print("Error: Could not connect to database")
+        print(e)
+        sys.exit(1)
+
 def login_data(key, file_location):
 	# connect to database
 	if os.path.isfile(file_location) == False:
@@ -118,28 +153,30 @@ def decrypt_data(encrypted_junk, key):
 
 			return cleartxt
 def main():
-	# get arguements
-	args = sys.argv[1:]
-	if len(args) != 3:
-		print("Usage: python decrypt.py <Decrypt Key> [--cookies || --passwords <DB File Location>]")
-		sys.exit(1)
-	key = args[0]
-	base64_key = base64.b64encode(key.encode())
-	option = args[1]
-	file_location = args[2]
-	
-	#base64 decode key
-	if option == "--cookies":
-		key = bytearray(base64.b64decode(base64_key).decode('utf-8').replace('\\x', ''), 'utf-8')
-		cookies(key, file_location)
-	elif option == "--passwords":
-		key = bytearray(base64.b64decode(base64_key).decode('utf-8').replace('\\x', ''), 'utf-8')
-		login_data(key, file_location)
-	elif option == "--firefox":
-		print("TO DO")
-	else:
-		print("Usage: python decrypt.py <Decrypt Key> [--cookies || --passwords <DB File Location>]")
-		sys.exit(1)
-	
+    # get arguments
+    args = sys.argv[1:]
+    if len(args) != 3:
+        print("Usage: python decrypt.py <Decrypt Key> [--cookies || --passwords || --cookie-editor <DB File Location>]")
+        sys.exit(1)
+    key = args[0]
+    base64_key = base64.b64encode(key.encode())
+    option = args[1]
+    file_location = args[2]
+    
+    # base64 decode key
+    key = bytearray(base64.b64decode(base64_key).decode('utf-8').replace('\\x', ''), 'utf-8')
+    
+    if option == "--cookies":
+        cookies(key, file_location)
+    elif option == "--passwords":
+        login_data(key, file_location)
+    elif option == "--cookie-editor":
+        cookies_for_editor(key, file_location)
+    elif option == "--firefox":
+        print("TO DO")
+    else:
+        print("Usage: python decrypt.py <Decrypt Key> [--cookies || --passwords || --cookie-editor <DB File Location>]")
+        sys.exit(1)
+    
 if __name__ == "__main__":
-	main()
+    main()

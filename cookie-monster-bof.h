d01cb4abbcff858fda37fd66985ec730ac45cd3d
Ubuntu <ubuntu@ubuntu0.env18>
2024-11-06 15:33:00 -0500

    app data decrypt added to bof, decrypt cookie DB broken in python

--- cookie-monster-bof.h
+++ cookie-monster-bof.h
@@ -87,3 +87,81 @@ enum PacketType {
     PACKET_FILE = 2,
     PACKET_CMD = 4
 };
+
+// https://gist.github.com/snovvcrash/caded55a318bbefcb6cc9ee30e82f824
+const CLSID Chrome_CLSID_Elevator = { 0x708860E0, 0xF641, 0x4611, {0x88, 0x95, 0x7D, 0x86, 0x7D, 0xD3, 0x67, 0x5B} };
+const IID Chrome_IID_IElevator    = { 0x463ABECF, 0x410D, 0x407F, {0x8A, 0xF5, 0x0D, 0xF3, 0x5A, 0x00, 0x5C, 0xC8} };
+
+typedef enum {
+    PROTECTION_NONE = 0,
+    PROTECTION_PATH_VALIDATION_OLD = 1,
+    PROTECTION_PATH_VALIDATION = 2,
+    PROTECTION_MAX = 3
+} ProtectionLevel;
+
+// https://fossies.org/dox/brave-core-1.71.121/x64_2elevation__service__idl_8h_source.html
+#ifndef DECLSPEC_XFGVIRT
+#if defined(_CONTROL_FLOW_GUARD_XFG)
+#define DECLSPEC_XFGVIRT(base, func) __declspec(xfg_virtual(base, func))
+#else
+#define DECLSPEC_XFGVIRT(base, func)
+#endif
+#endif
+ 
+typedef struct IElevator IElevator;
+
+typedef struct IElevatorVtbl
+    {
+        BEGIN_INTERFACE
+        
+        DECLSPEC_XFGVIRT(IUnknown, QueryInterface)
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            IElevator * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        DECLSPEC_XFGVIRT(IUnknown, AddRef)
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            IElevator * This);
+        
+        DECLSPEC_XFGVIRT(IUnknown, Release)
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            IElevator * This);
+        
+        DECLSPEC_XFGVIRT(IElevator, RunRecoveryCRXElevated)
+        HRESULT ( STDMETHODCALLTYPE *RunRecoveryCRXElevated )( 
+            IElevator * This,
+            /* [string][in] */ const WCHAR *crx_path,
+            /* [string][in] */ const WCHAR *browser_appid,
+            /* [string][in] */ const WCHAR *browser_version,
+            /* [string][in] */ const WCHAR *session_id,
+            /* [in] */ DWORD caller_proc_id,
+            /* [out] */ ULONG_PTR *proc_handle);
+        
+        DECLSPEC_XFGVIRT(IElevator, EncryptData)
+        HRESULT ( STDMETHODCALLTYPE *EncryptData )( 
+            IElevator * This,
+            /* [in] */ ProtectionLevel protection_level,
+            /* [in] */ const BSTR plaintext,
+            /* [out] */ BSTR *ciphertext,
+            /* [out] */ DWORD *last_error);
+        
+        DECLSPEC_XFGVIRT(IElevator, DecryptData)
+        HRESULT ( STDMETHODCALLTYPE *DecryptData )( 
+            IElevator * This,
+            /* [in] */ const BSTR ciphertext,
+            /* [out] */ BSTR *plaintext,
+            /* [out] */ DWORD *last_error);
+        
+        DECLSPEC_XFGVIRT(IElevator, InstallVPNServices)
+        HRESULT ( STDMETHODCALLTYPE *InstallVPNServices )( 
+            IElevator * This);
+        
+        END_INTERFACE
+    } IElevatorVtbl;
+ 
+    interface IElevator
+    {
+        CONST_VTBL struct IElevatorVtbl *lpVtbl;
+    };
